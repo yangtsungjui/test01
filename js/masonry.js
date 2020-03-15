5a9cba480b717f1781100ef31ee3aab27961b216
unknown <raymondyang0319@gmail.com>
2020-03-15 20:02:21 +0800

    Add index

--- js/masonry.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-  for (var i = 0; i < 25; i++) {
-    $('#photos').append();
-  }
-});
